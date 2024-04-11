952332659eca55879b7df7dc84bc3ce0d89a9164
1060132 <151944916+1060132@users.noreply.github.com>
2024-04-11 12:51:05 +0200

    zeugs halt

--- writeData.py
+++ writeData.py
@@ -1,6 +1,12 @@
 import json
 
+
+import json
+
 def add_building_to_json(json_file_path, x_coordinate, y_coordinate, building_type):
+        housing_count = 0
+        farm_count  = 0
+        factory_count = 0
         with open(json_file_path, 'r') as file:
             data = json.load(file)
 
@@ -15,13 +21,27 @@ def add_building_to_json(json_file_path, x_coordinate, y_coordinate, building_ty
             "y-Coordinate": y_coordinate,
             "type": building_type
         }
-
         data[new_building_name] = new_building
 
+        if building_type == "Haus":
+             housing_count+=1
+        elif building_type == "Farm":
+             farm_count+=1
+        elif building_type == "Fabrik":
+             factory_count+=1
+        
+        counter = {
+             "House": housing_count,
+             "Farm": farm_count,
+             "Factories": factory_count
+        }
+        data["count"] = counter
         with open(json_file_path, 'w') as file:
             json.dump(data, file, indent=4)
 
-add_building_to_json("./first-page.json", 4, 2, "Mine")
+
+
+add_building_to_json("./first-page.json", 1, 6, "Haus")
 
 
 
@@ -37,3 +57,7 @@ def remove_building(json_file_path, x_coordinate, y_coordinate, building_type):
 
         with open(json_file_path, "w") as file:
             json.dump(data, file, indent=4)
+
+
+
+ 
